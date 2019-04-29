433292b26af2ad24a5166c39219353f2443c8b99
fuaad001 <husseinfuaad@hotmail.com>
2019-04-29 07:12:05 +0300

    Specify all configurations

--- config.py
+++ config.py
@@ -8,6 +8,15 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'New Blog'
+    SENDER_EMAIL = 'husseinfuaadm@gmail.com'
+
     # simple mde configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
